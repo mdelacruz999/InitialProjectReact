5769b50f96353f61b1f86cf07ab4ade14d5081d4
Moisés <delacruzmoises80@gmail.com>
2024-05-15 20:57:07 -0500

    creating a component that receives a text and then updates it on the web app

--- src/Component.js
+++ src/Component.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Component() {
+
+  const [text, setText] = useState()
+
+  const [updated, setUpdated] = useState()
+  
+  const textOnChange = (event) => {
+    
+    setText(event.target.value)
+  }
+  const buttonOnClick = () => {
+    
+    setUpdated(text)
+  }
+  
   return (
     <div>
-      My component
+     <input type="text" value={text} onChange={textOnChange}/>
+     <button onClick={buttonOnClick}>Actualizar</button>
+     <p>Texto input: {text}</p>
+     <p>Texto Actualizado: {updated}</p>
     </div>
   )
 }
